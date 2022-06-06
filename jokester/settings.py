749fa54976bbd563176dc60a151ecfc635ec1f4f
dzoladz <derek@derekzoladz.com>
2022-06-05 21:38:16 -0400

    ADD: setup dotenv for SECRET_KEY

--- jokester/settings.py
+++ jokester/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3bj@alcrex*#8&zxcz1%^0)7@q6$o!gpum)bn39$c+$y#*fosv'
+# SECRET_KEY = 'django-insecure-3bj@alcrex*#8&zxcz1%^0)7@q6$o!gpum)bn39$c+$y#*fosv'
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jokester2022.herokuapp.com']
 
