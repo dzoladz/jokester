6ecc13aaed8e37aa950909ba5b3b3627fcc57c5e
dzoladz <derek@derekzoladz.com>
2022-06-05 20:51:59 -0400

    UPDATE: add whitenoise to serve static files

--- jokester/settings.py
+++ jokester/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware'
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'jokester.urls'
